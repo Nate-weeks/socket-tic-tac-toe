8de61cc4a0a81a23026637c46a8d1deb51165767
Nathaniel Weeks <Nate@Nathaniels-MacBook-Pro.local>
2018-05-04 14:08:38 -0400

    building a chat client

---
+++ test-client.py
@@ -0,0 +1,20 @@
+# Import socket module
+import socket
+
+# Create a socket object
+s = socket.socket()
+
+# Define the port on which you want to connect
+port = 12345
+
+# connect to the server on local computer
+s.connect(('127.0.0.1', port))
+
+greeting = s.recv(1024)
+
+print greeting
+
+while true:
+    chat = raw_input("what would you like to say?")
+    s.send(chat)
+    response = s.recv(1024)
---
+++ testchatgame.py
@@ -0,0 +1,6 @@
+class chat(object):
+    def __init__(self, clients):
+        self.clients = clients
+
+    def broadcast(msg):
+        
